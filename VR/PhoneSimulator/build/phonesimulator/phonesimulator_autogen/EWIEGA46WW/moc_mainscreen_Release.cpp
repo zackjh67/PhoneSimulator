a769efc515367fa51c09ca5b094f400579dc4ee4
Zachary Hern <zackjh67@gmail.com>
2018-03-23 18:25:42 -0400

    Added signals from GUI

--- VR/PhoneSimulator/build/phonesimulator/phonesimulator_autogen/EWIEGA46WW/moc_mainscreen_Release.cpp
+++ VR/PhoneSimulator/build/phonesimulator/phonesimulator_autogen/EWIEGA46WW/moc_mainscreen_Release.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainScreen_t {
-    QByteArrayData data[8];
-    char stringdata0[121];
+    QByteArrayData data[9];
+    char stringdata0[134];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,17 +30,18 @@ struct qt_meta_stringdata_MainScreen_t {
 static const qt_meta_stringdata_MainScreen_t qt_meta_stringdata_MainScreen = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainScreen"
-QT_MOC_LITERAL(1, 11, 13), // "onNotifPosted"
-QT_MOC_LITERAL(2, 25, 0), // ""
-QT_MOC_LITERAL(3, 26, 12), // "notification"
-QT_MOC_LITERAL(4, 39, 21), // "on_sendButton_clicked"
-QT_MOC_LITERAL(5, 61, 37), // "on_notificationListWidget_ite..."
-QT_MOC_LITERAL(6, 99, 16), // "QListWidgetItem*"
-QT_MOC_LITERAL(7, 116, 4) // "item"
+QT_MOC_LITERAL(1, 11, 12), // "showKeyboard"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 13), // "onNotifPosted"
+QT_MOC_LITERAL(4, 39, 12), // "notification"
+QT_MOC_LITERAL(5, 52, 21), // "on_sendButton_clicked"
+QT_MOC_LITERAL(6, 74, 37), // "on_notificationListWidget_ite..."
+QT_MOC_LITERAL(7, 112, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(8, 129, 4) // "item"
 
     },
-    "MainScreen\0onNotifPosted\0\0notification\0"
-    "on_sendButton_clicked\0"
+    "MainScreen\0showKeyboard\0\0onNotifPosted\0"
+    "notification\0on_sendButton_clicked\0"
     "on_notificationListWidget_itemClicked\0"
     "QListWidgetItem*\0item"
 };
@@ -52,22 +53,28 @@ static const uint qt_meta_data_MainScreen[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   34,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x0a /* Public */,
-       4,    0,   32,    2, 0x08 /* Private */,
-       5,    1,   33,    2, 0x08 /* Private */,
+       3,    1,   35,    2, 0x0a /* Public */,
+       5,    0,   38,    2, 0x08 /* Private */,
+       6,    1,   39,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::QString,    4,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 6,    7,
+    QMetaType::Void, 0x80000000 | 7,    8,
 
        0        // eod
 };
@@ -78,10 +85,21 @@ void MainScreen::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         MainScreen *_t = static_cast<MainScreen *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->onNotifPosted((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 1: _t->on_sendButton_clicked(); break;
-        case 2: _t->on_notificationListWidget_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 0: _t->showKeyboard(); break;
+        case 1: _t->onNotifPosted((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 2: _t->on_sendButton_clicked(); break;
+        case 3: _t->on_notificationListWidget_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (MainScreen::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainScreen::showKeyboard)) {
+                *result = 0;
+                return;
+            }
         }
     }
 }
@@ -111,14 +129,20 @@ int MainScreen::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
+}
+
+// SIGNAL 0
+void MainScreen::showKeyboard()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
 }
 QT_END_MOC_NAMESPACE
--- VR/PhoneSimulator/build/phonesimulator/phonesimulator_autogen/EWIEGA46WW/moc_vrcontroller_Release.cpp
+++ VR/PhoneSimulator/build/phonesimulator/phonesimulator_autogen/EWIEGA46WW/moc_vrcontroller_Release.cpp
@@ -20,8 +20,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_vrController_t {
-    QByteArrayData data[5];
-    char stringdata0[63];
+    QByteArrayData data[6];
+    char stringdata0[76];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,11 +34,12 @@ QT_MOC_LITERAL(0, 0, 12), // "vrController"
 QT_MOC_LITERAL(1, 13, 14), // "OnSceneChanged"
 QT_MOC_LITERAL(2, 28, 0), // ""
 QT_MOC_LITERAL(3, 29, 13), // "QList<QRectF>"
-QT_MOC_LITERAL(4, 43, 19) // "OnTimeoutPumpEvents"
+QT_MOC_LITERAL(4, 43, 19), // "OnTimeoutPumpEvents"
+QT_MOC_LITERAL(5, 63, 12) // "showKeyboard"
 
     },
     "vrController\0OnSceneChanged\0\0QList<QRectF>\0"
-    "OnTimeoutPumpEvents"
+    "OnTimeoutPumpEvents\0showKeyboard"
 };
 #undef QT_MOC_LITERAL
 
@@ -48,7 +49,7 @@ static const uint qt_meta_data_vrController[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -56,11 +57,13 @@ static const uint qt_meta_data_vrController[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   24,    2, 0x0a /* Public */,
-       4,    0,   27,    2, 0x0a /* Public */,
+       1,    1,   29,    2, 0x0a /* Public */,
+       4,    0,   32,    2, 0x0a /* Public */,
+       5,    0,   33,    2, 0x0a /* Public */,
 
  // slots: parameters
     QMetaType::Void, 0x80000000 | 3,    2,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -74,6 +77,7 @@ void vrController::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         switch (_id) {
         case 0: _t->OnSceneChanged((*reinterpret_cast< const QList<QRectF>(*)>(_a[1]))); break;
         case 1: _t->OnTimeoutPumpEvents(); break;
+        case 2: _t->showKeyboard(); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
@@ -115,13 +119,13 @@ int vrController::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
